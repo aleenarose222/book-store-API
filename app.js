52d7e1a6a18a94cef008e0851099c989759e02e3
aleenarose222 <aleenarose222@gmail.com>
2024-12-09 23:40:44 +0530

    first commit

--- app.js
+++ app.js
@@ -25,3 +25,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
